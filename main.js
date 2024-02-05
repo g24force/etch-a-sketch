9927609d786569a7bc54393701c1a17726cc1b53
g24force <102439759+g24force@users.noreply.github.com>
2024-02-05 16:28:57 +0000

    Create 16x16 grid

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let i = 0;
 const container = document.querySelector('#container');
-while (i < 16) {
+while (i < 256) {
     const div = document.createElement('div');
+    div.textContent ='1';
     container.appendChild(div);
     i++;
 };
