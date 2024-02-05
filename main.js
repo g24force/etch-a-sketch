811034b062101e475550b01aa072c201492e2fad
g24force <102439759+g24force@users.noreply.github.com>
2024-02-05 16:59:48 +0000

    Add border and div number for better visualization

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let i = 0;
 const container = document.querySelector('#container');
 while (i < 256) {
     const div = document.createElement('div');
-    div.textContent ='1';
+    div.textContent = i + 1;
     container.appendChild(div);
     i++;
 };
