462f29ff0daa23c22a86737d74d3c7f31ad51e68
g24force <102439759+g24force@users.noreply.github.com>
2024-02-06 00:22:35 +0000

    WIP Make the divs squared

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const container = document.querySelector('#container');
 while (i < 256) {
     const div = document.createElement('div');
     div.textContent = i + 1;
+    div.classList.add('cell');
     container.appendChild(div);
     i++;
 };
