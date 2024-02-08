757b385fc98b5033e1c271bfade8126382c7cca8
g24force <102439759+g24force@users.noreply.github.com>
2024-02-08 00:44:28 +0000

    Add hover effect to divs

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let i = 0;
+let i = 1;
+let columns = 16;
+let flexBasisAddValue = Math.floor(1 / columns * 100) + "%";
 const container = document.querySelector('#container');
-while (i < 256) {
+while (i <= 16 * 16) {
     const div = document.createElement('div');
-    div.textContent = i + 1;
+    /* div.textContent = i; */
+    div.style.flexBasis = flexBasisAddValue;
     div.classList.add('cell');
     container.appendChild(div);
     i++;
